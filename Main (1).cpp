a1c45640a247866a98cb8e3aa9c7900f997c9c3a
inc4dge <380605275@qq.com>
2025-09-22 20:37:04 +0800

    Add files via upload

---
+++ Main (1).cpp
@@ -0,0 +1,15 @@
+#include<cstdio>
+int main()
+{
+    int x,y,z,sum;
+    float average;
+    scanf("%d %d %d",&x,&y,&z);
+    sum=x+y+z;
+    average=(float)sum/3;
+    printf("%d\n%.2f",sum,average);
+    return 0;
+}
+
+
+
+
